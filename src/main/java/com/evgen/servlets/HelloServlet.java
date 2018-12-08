c0d781d83f673c89d435a4e8b73230997f2dac0b
evgen <izerus@gmail.com>
2018-12-08 21:47:46 +0200

    sdtjhjhhgj

---
+++ src/main/java/com/evgen/servlets/HelloServlet.java
@@ -0,0 +1,19 @@
+package com.evgen.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Writer;
+
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        PrintWriter writer = resp.getWriter();
+        writer.write("Hello world");
+        writer.println("Hello world");
+    }
+}
